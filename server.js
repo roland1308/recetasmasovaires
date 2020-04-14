27a0ab26b4ddd0bf538948b2797c6da14ebb871b
Renato Acciardi <a.renato@gmail.com>
2020-04-14 10:53:23 +0200

    first commit

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/recipes', require('./routes/recipes'))
 if (process.env.NODE_ENV === "production") {
     // Set static folder
     app.use(express.static("./client/build"));
-    app.get("*", (req, res) => {
+    app.get("/", (req, res) => {
         res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
     });
 }
