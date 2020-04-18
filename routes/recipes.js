1169e3431ea813113d1c5b2085e6ce039ee17a3c
Renato Acciardi <a.renato@gmail.com>
2020-04-18 09:33:36 +0200

    Adding Cloudinary

--- routes/recipes.js
+++ routes/recipes.js
@@ -59,14 +59,14 @@ router.post('/add', (req, res) => {
 
 /*add photo to uploads folder*/
 router.post("/addphoto", upload.single("picture"), (req, res) => {
-    const path = "./" + req.file.path
+    const path = req.file.path
     console.log(path)
-    cloudinary.uploader.upload(path, { public_id: req.file.originalname }, function (err, result) {
+    cloudinary.v2.uploader.upload(path, { public_id: req.file.originalname }, function (err, result) {
         if (err) {
-            return res.send("err")
+            res.send("err")
         }
         // fs.unlinkSync(req.file.path)
-        return res.send(result.secure_url)
+        res.send(result.secure_url)
     })
 });
 
